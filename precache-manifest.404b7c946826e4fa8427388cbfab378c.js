3f331f79b9c638aa5ce8872d47aa7f9e8290b81b
oleg5913 <oleg1906@gmail.com>
2020-11-01 03:54:54 +0300

    v.1.0

---
+++ precache-manifest.404b7c946826e4fa8427388cbfab378c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6aae9def14b3dfde16984b92265949d0",
+    "url": "/e-store_prototype/index.html"
+  },
+  {
+    "revision": "3db497b7a212a54b5bd3",
+    "url": "/e-store_prototype/static/css/main.a45b1a9a.chunk.css"
+  },
+  {
+    "revision": "0675be32f6bb43880a3d",
+    "url": "/e-store_prototype/static/js/2.39ac385e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/e-store_prototype/static/js/2.39ac385e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3db497b7a212a54b5bd3",
+    "url": "/e-store_prototype/static/js/main.e42e4a7d.chunk.js"
+  },
+  {
+    "revision": "1e4e6a180a4f395edc0a",
+    "url": "/e-store_prototype/static/js/runtime-main.2275eccc.js"
+  },
+  {
+    "revision": "d6476c0987b0bc9bae7952344e9bec5d",
+    "url": "/e-store_prototype/static/media/19572.d6476c09.ttf"
+  },
+  {
+    "revision": "16efd34212812ac4559f66089822ce8c",
+    "url": "/e-store_prototype/static/media/Comfortaa-Light.16efd342.ttf"
+  },
+  {
+    "revision": "a0e4dc9dc21373e9c022d541f20613a7",
+    "url": "/e-store_prototype/static/media/history.a0e4dc9d.png"
+  },
+  {
+    "revision": "5de4f3fccc5bb4990e1344c7e02883c4",
+    "url": "/e-store_prototype/static/media/manCatalog.5de4f3fc.png"
+  },
+  {
+    "revision": "33306ddd25901a332f115675788096d4",
+    "url": "/e-store_prototype/static/media/womanCatalog.33306ddd.png"
+  }
+]);
